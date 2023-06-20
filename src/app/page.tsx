55cc8588db589d96e78e1b9df60ad3b4ad04bf61
impolska742 <vaibhav19bhardwaj@gmail.com>
2023-06-20 11:34:32 +0530

    logos and header added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Logo from "@/assets/xpense-full.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-yellow-500">Xpense.</h1>
+    <main className="flex w-full min-h-screen flex-col items-center justify-start">
+      <header className="w-full bg-primaryDark align-center bg-red-300 flex justify-center">
+        <div className="w-full max-w-5xl">
+          <Image src={Logo} alt="Xpense Logo" width={250} />
+        </div>
+      </header>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      primaryDark: "#1f1f1f",
+      secondaryDark: "#434242",
+      ternaryDark: "#eca967",
+      primaryLight: "#f5cd92",
+      secondaryLight: "#f2c183",
     },
   },
   plugins: [],
-}
+};
